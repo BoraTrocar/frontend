bc05aecedfb0b7b2765b0633b32978386844ebc8
Flávio Almeida <112271774+FlavioAlmeidaa@users.noreply.github.com>
2025-06-09 23:25:45 -0300

    teste

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
-  production: false,
+  production: true,
+  //apiUrl: 'http://localhost:8090/',
+  firebase: {
+    apiKey: 'AIzaSyBmH__NnQBHzXIVNzx0AjpqH6SOg_zjW0g',
+    authDomain: 'boratrocar-7e01f.firebaseapp.com',
+    databaseURL: 'https://boratrocar-7e01f-default-rtdb.firebaseio.com',
+    projectId: 'boratrocar-7e01f',
+    storageBucket: 'boratrocar-7e01f.appspot.com',
+    messagingSenderId: '254748397046',
+    appId: '1:254748397046:android:2eff7e364002f06d384bd7',
+  },
+
   apiUrl: 'https://boratrocar.company:8090/',
-  //apiUrl: 'http://45.55.202.40:8090/'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   //apiUrl: 'http://localhost:8090/',
   firebase: {
     apiKey: 'AIzaSyBmH__NnQBHzXIVNzx0AjpqH6SOg_zjW0g',
